391a2aa2f10f07b7020cc2c2a06e9c2ce0fb2323
Ing. Carlos Ali Rodriguez <ali2164@hotmail.com>
2024-01-19 21:43:19 -0600

    Agregar archivos de proyecto.

---
+++ TestGraficos/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TestGraficos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(265, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1029, 793);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(265, 46);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(185, 34);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Dibujar linea";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(522, 46);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(200, 34);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "Dibuja Circulo";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(795, 46);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(233, 34);
+            this.simpleButton3.TabIndex = 3;
+            this.simpleButton3.Text = "Dibuja Rectangulo";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1080, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Dibujar polilinea";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2207, 1332);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestGraficos/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGraficos
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bitmap;
+        Graphics grafico;
+        Pen plumaNegra;
+        Pen plumaRoja;
+        Pen plumaAzul;
+     
+        bool dibujarPolilinea=false;
+        bool primerClick = true;
+
+        float xInicio = 0;
+        float yInicio = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap= new Bitmap(pictureBox1.Width,pictureBox1.Height);
+
+            pictureBox1.Image= bitmap;
+
+            grafico= Graphics.FromImage(bitmap);
+
+            //CREAMOS LAS PLUMAS:
+             plumaNegra = new Pen(Color.Black, 2f);
+            plumaRoja = new Pen(Color.Red, 4f);
+            plumaRoja = new Pen(Color.Red, 4f);
+            plumaAzul = new Pen(Color.Blue, 5f);
+
+
+
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            grafico.DrawLine(plumaNegra, 10, 10, 200, 200);
+            pictureBox1.Invalidate();
+          
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            grafico.DrawEllipse(plumaRoja,50,50,60,60);
+            pictureBox1.Invalidate();
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            grafico.DrawRectangle(plumaAzul, 30, 30, 80, 50);
+            pictureBox1.Invalidate();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (dibujarPolilinea == false) { return; }
+
+            if (primerClick)
+            {
+                xInicio=e.Location.X;
+                yInicio=e.Location.Y;
+
+                //DIBUJAMOS PUNTO DE REFERENCIA
+                grafico.DrawEllipse(plumaRoja, xInicio, yInicio, 5, 5);
+                pictureBox1.Invalidate();
+
+                primerClick = false;
+
+            }
+            else
+            {
+                float xFinal= e.Location.X;
+                float yFinal= e.Location.Y;
+
+                grafico.DrawEllipse(plumaRoja, xFinal, yFinal, 5, 5);
+                grafico.DrawLine(plumaAzul,xInicio,yInicio,xFinal,yFinal);
+
+                //ACTUALIZAMOS LAS COORDENADAS DE INICIO
+                xInicio = xFinal;
+                yInicio=yFinal; 
+
+                pictureBox1.Invalidate();
+
+            }
+
+
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //PARA CREAR NUEVA POLILINEA
+            primerClick = true;
+
+            dibujarPolilinea = true;
+
+            MessageBox.Show("Dibúje polilínea");
+
+        }
+    }
+}
